043d87af38ff9c471ec513426bd2bdd136ebe6b4
xupeng72083 <xupeng72083@gmail.com>
2021-10-13 23:47:18 -0400

    remove warning message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import React from 'react';
 import { AppLayout } from '../components';
 import MainIntro from '@/components/home/mainIntro';
 import WorkingProcess from '@/components/home/workingProcess';
 import FaqForm from '@/components/home/faqForm';
 import FaqSection from '@/components/home/faqSection';
-import ReviewsPlatform from '@/components/home/reviewsPlatform';
+const ReviewsPlatform = dynamic(() => import('@/components/home/reviewsPlatform'), { ssr: false });
 
 const Home: NextPage = () => (
   <AppLayout>
