94bab67871d19659a7fdd5acd59f9db1dc520def
xupeng72083 <xupeng72083@gmail.com>
2021-11-16 02:06:04 -0500

    Add contact us and about page

--- components/contactUs/index.tsx
+++ components/contactUs/index.tsx
@@ -10,11 +10,14 @@ const ContactUs: React.FC = () => (
             <Image src="/images/Contacts/contacts-bg.jpg" width={1328} height={659} alt="" />
           </div>
           <h1>
-            We are an
+            {'We are an '}
             <br />
-            online passport service company
+            {'online passport service company'}
             <br />
-            based in <span>New York</span> and <span>Los Angeles</span>.
+            {'based in '}
+            <span>{'New York'}</span>
+            {' and '}
+            <span>{'Los Angeles'}</span>.
           </h1>
         </div>
       </div>
@@ -25,10 +28,10 @@ const ContactUs: React.FC = () => (
         <div className="row">
           <div className="col-12">
             <div className="title">
-              <h2>Contact Us</h2>
+              <h2>{'Contact Us'}</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sem tellus
-                natoque sed sagittis nisl, nullam.
+                {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sem tellus
+                  natoque sed sagittis nisl, nullam.`}
               </p>
             </div>
           </div>
@@ -36,34 +39,34 @@ const ContactUs: React.FC = () => (
             <div className="text">
               <ul>
                 <li>
-                  <h3>PHONE</h3>
+                  <h3>{'PHONE'}</h3>
                   <p>
-                    <a href="tel:+1234567890">+1 234 567 890</a>
+                    <a href="tel:+1234567890">{'+1 234 567 890'}</a>
                   </p>
                 </li>
                 <li>
-                  <h3>EMAIL</h3>
+                  <h3>{'EMAIL'}</h3>
                   <p>
-                    <a href="mailto:customservice@abc.com">customservice@abc.com</a>
+                    <a href="mailto:customservice@abc.com">{'customservice@abc.com'}</a>
                   </p>
                 </li>
                 <li>
-                  <h3>OFFICE</h3>
+                  <h3>{'OFFICE'}</h3>
                   <p>
-                    Mon - Fri: 9am - 6pm
+                    {'Mon - Fri: 9am - 6pm'}
                     <br />
-                    Sat & Sun: 10am - 4pm
+                    {'Sat & Sun: 10am - 4pm'}
                   </p>
                   <p>
-                    248 N Rodeo Dr, Beverly Hills,
+                    {'248 N Rodeo Dr, Beverly Hills,'}
                     <br />
-                    CA 90210
+                    {'CA 90210'}
                   </p>
                 </li>
               </ul>
               <div className="btn">
                 <a>
-                  Get Directions
+                  {'Get Directions'}
                   <span className="icon-longarrow" />
                 </a>
               </div>
@@ -72,28 +75,28 @@ const ContactUs: React.FC = () => (
           <div className="col-12 col-lg-8">
             <form>
               <label className="form-field col-12 col-lg-6">
-                <span>First Name*</span>
+                <span>{'First Name*'}</span>
                 <input type="text" placeholder="First Name" />
               </label>
               <label className="form-field col-12 col-lg-6">
-                <span>Last Name*</span>
+                <span>{'Last Name*'}</span>
                 <input type="text" placeholder="Last Name" />
               </label>
               <label className="form-field col-12 col-lg-6">
-                <span>Email*</span>
+                <span>{'Email *'}</span>
                 <input type="email" placeholder="Email Address" />
               </label>
               <label className="form-field col-12 col-lg-6">
-                <span>Phone Number*</span>
+                <span>{'Phone Number*'}</span>
                 <input type="tel" placeholder="Phone Number" />
               </label>
               <label className="form-field col-12">
-                <span>Leave A Message</span>
+                <span>{'Leave A Message'}</span>
                 <textarea placeholder="Message" />
               </label>
               <div className="form-submit submit-btn col-12">
                 <button type="button" className="main-btn">
-                  Send
+                  {'Send'}
                 </button>
               </div>
             </form>
