08e13e9fed1f940a1d3aa96a838e69e0f957fb3a
xupeng72083 <xupeng72083@gmail.com>
2021-11-25 02:51:10 -0500

    home ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next';
-import dynamic from 'next/dynamic';
 import React from 'react';
 import { AppLayout } from '../components';
 import { NextSeo } from 'next-seo';
 import { SEO } from '../constants';
-
-const Home = dynamic(() => import('@/components/home'), { ssr: false });
+import Home from '@/components/home';
 
 const HomePage: NextPage = () => (
   <>
