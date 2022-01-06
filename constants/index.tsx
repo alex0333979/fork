9af3432b83a33ef2a7b47080867a3dde2c527568
xupeng72083 <xupeng72083@gmail.com>
2022-01-06 23:45:22 +0800

    Add passport images for US and UK

--- constants/index.tsx
+++ constants/index.tsx
@@ -360,3 +360,45 @@ export const CHECKLIST = [
   'Eyes Open',
   'Pupils'
 ];
+
+export const US_PASSPORT_IMAGES = [
+  {
+    name: 'Visa',
+    image: 'us-visa.png'
+  },
+  {
+    name: 'USCIS Forms Photo',
+    image: 'us-uscis.png'
+  },
+  {
+    name: 'Green Card (Permanent Resident)',
+    image: 'us-green-card.png'
+  },
+  {
+    name: 'NY Gun License',
+    image: 'us-gun-license.png'
+  }
+];
+
+export const UK_PASSPORT_IMAGES = [
+  {
+    name: 'Passport',
+    image: 'uk-passport-photo.png'
+  },
+  {
+    name: 'Passport (ONLINE)',
+    image: 'uk-passport-photo.png'
+  },
+  {
+    name: 'Driving Licence',
+    image: 'uk-driving-license.png'
+  },
+  {
+    name: 'ID / residence card',
+    image: 'uk-res-card.png'
+  },
+  {
+    name: 'Visa',
+    image: 'uk-visa.jpeg'
+  }
+];
