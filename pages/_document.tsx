62e184acfce1bb80007da35c8204ba9c855a9b42
fatpig0416 <fatpig0416@gmail.com>
2022-03-30 22:12:56 +0800

    ad UET tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
@@ -107,6 +108,20 @@ class MyDocument extends Document {
           />
           <meta property="og:image" content="template.png" />
           <meta property="og:locale" content="us_EN" />
+          {/* Microsoft UET tag */}
+          <script
+            id="load-uet"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[] ,f=function(){var o={ti:"148009486"}; o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")} ,n=d.createElement(t),n.src=r,n.async=1,n.onload=n .onreadystatechange=function() {var s=this.readyState;s &&s!=="loaded"&& s!=="complete"||(f(),n.onload=n. onreadystatechange=null)},i= d.getElementsByTagName(t)[0],i. parentNode.insertBefore(n,i)})(window,document,"script"," //bat.bing.com/bat.js","uetq");`
+            }}
+          />
+          <script
+            id="trigger-uet"
+            dangerouslySetInnerHTML={{
+              __html: `window.uetq = window.uetq || [];
+                window.uetq.push('event', 'page_view', { page_path: '/' + window.location.pathname });`
+            }}
+          />
         </Head>
         <body>
           <Main />
