7151f6673dbc6fd288428e3171674ccb6f38e2ba
fatpig0416 <fatpig0416@gmail.com>
2022-05-07 00:42:31 +0800

    cancel test deploy

--- components/elements/takePhotoModal.tsx
+++ components/elements/takePhotoModal.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ModalContainer from '@/components/elements/modalContainer';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
+import { showError } from '@/lib/utils/toast';
+import { createFileFromBase64 } from '@/lib/utils/downloadFromBase64';
 
 interface TakePhotoProps {
   open: boolean;
@@ -9,10 +12,45 @@ interface TakePhotoProps {
   takePhoto: (file: File) => void;
 }
 
-const TakePhotoModal: React.FC<TakePhotoProps> = ({ open, closeTakePhoto }) => (
-  <ModalContainer open={open} closeModal={() => closeTakePhoto()}>
-    <div>Deploy test</div>
-  </ModalContainer>
-);
+const TakePhotoModal: React.FC<TakePhotoProps> = ({
+  open,
+  closeTakePhoto,
+  takePhoto,
+  idealFacingMode
+}) => {
+  const handleTakePhotoAnimationDone = useCallback(
+    (dataUri: string) => {
+      const file = createFileFromBase64(dataUri);
+      takePhoto(file);
+    },
+    [takePhoto]
+  );
+
+  const handleCameraError = useCallback((error: Error) => {
+    console.log(error);
+    showError(error.message);
+  }, []);
+
+  return (
+    <ModalContainer open={open} closeModal={() => closeTakePhoto()}>
+      {open && (
+        <Camera
+          onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
+          idealFacingMode={
+            idealFacingMode === FACING_MODES.ENVIRONMENT
+              ? FACING_MODES.ENVIRONMENT
+              : FACING_MODES.USER
+          }
+          imageType={IMAGE_TYPES.PNG}
+          idealResolution={{ width: 2000, height: 2000 }}
+          isDisplayStartCameraError={false}
+          onCameraError={handleCameraError}
+          isFullscreen={false}
+          isImageMirror={idealFacingMode !== FACING_MODES.ENVIRONMENT}
+        />
+      )}
+    </ModalContainer>
+  );
+};
 
 export default TakePhotoModal;
