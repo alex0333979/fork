7c0747ad7bc37899c7e18550fdc69237f1d4f337
Lior Lindvor <lior.lindvor@gmail.com>
2022-06-21 01:04:55 +0300

    fix double fire woopra

--- components/layout/photoLayout.tsx
+++ components/layout/photoLayout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PhotoHeader from '@/components/layout/photoHeader'
-import WoopraScript from '@/components/scripts/woopra'
 
 interface AppLayoutProps {
   children: React.ReactNode
@@ -9,7 +8,6 @@ const PhotoLayout: React.FC<AppLayoutProps> = ({ children }) => (
   <>
     <PhotoHeader />
     <main>{children}</main>
-    <WoopraScript />
   </>
 )
 
