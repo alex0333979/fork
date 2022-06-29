1ee222f48113072e7509ff01fd7ad1ff36fcb26c
Lior Lindvor <lior.lindvor@gmail.com>
2022-06-29 17:35:31 +0300

    fix lint

--- components/checkout/deliveryMethod.tsx
+++ components/checkout/deliveryMethod.tsx
@@ -83,9 +83,7 @@ const DeliveryMethod: React.FC = () => {
               <span className="option">{'Digital Photo (Only)'}</span>
               <span className="slider" />
               <span className="option" data-status={'Recommended'}>
-                <b>
-                  {'Print & Ship To My Address (Free Delivery)'}
-                </b>
+                <b>{'Print & Ship To My Address (Free Delivery)'}</b>
               </span>
             </span>
           </label>
