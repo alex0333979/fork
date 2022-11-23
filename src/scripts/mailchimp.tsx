a000fdbaf9a89286f45fb0f3a278871132b60b5a
Lior Lindvor <lior.lindvor@gmail.com>
2022-11-23 18:31:09 +0200

    Update mailchimp.tsx

--- src/scripts/mailchimp.tsx
+++ src/scripts/mailchimp.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const MailChimpScript: React.FC = () => (
   <>
     <script
-      id="mailchimp"
+      id="mcjs"
       dangerouslySetInnerHTML={{
         __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/90d49cd3b92c3258f92ad1ff1/8485d1ffa4f3784e20cfa7b8e.js");`,
       }}
