57fe2968f773ed0ef349db7fd822a53dbb116470
fatpig0416 <fatpig0416@gmail.com>
2022-11-24 16:41:05 +0800

    fix dropdown issue

--- src/modules/checkout/deliveryMethod/expeditingServiceQuestions.tsx
+++ src/modules/checkout/deliveryMethod/expeditingServiceQuestions.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import classNames from 'classnames'
 import countryList from 'react-select-country-list'
 
 import RadioElement from '@/components/elements/radioElement'
+import { useClickOutside } from '@/hooks'
 
 interface Props {
   values: Record<string, string>
+  onChange: (q: string, a: string) => void
+}
+
+interface QuestionRowProps {
+  q: IQuestion
+  qIndex: number
+  opened: boolean
+  values: Record<string, string>
+  onToggle: (index: number, close?: boolean) => void
   onChange: (q: string, a: string) => void
 }
 
@@ -89,41 +99,92 @@ const questions: IQuestion[] = [
   },
 ]
 
-const ExpeditingServiceQuestions: React.FC<Props> = ({ values, onChange }) => (
-  <div className="form-fields">
-    <div className="select-list">
-      {questions.map((q, qIndex) => (
-        <div key={`q-${qIndex}`} className="group">
-          <div className="group-label">
-            <p>{`${qIndex + 1}. ${q.question}`}</p>
-          </div>
-          <div className="group-select">
-            <div className="label">
-              <p>{values[q.question] || q.placeholder}</p>
-            </div>
-            <div className="option">
-              {q.answers.map((a, aIndex) => (
-                <div key={`a-${aIndex}`} className="expediting-answers">
-                  <RadioElement<string>
-                    className={classNames('expediting-answer-option', {
-                      // @ts-ignore
-                      'option-selected': values[q.question] === a.value,
-                    })}
-                    label={a.label}
-                    name={q.question}
-                    // @ts-ignore
-                    selected={values[q.question] === a.value}
-                    value={a.value}
-                    onSelect={(v: string) => onChange(q.question, v)}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
+const QuestionRow: React.FC<QuestionRowProps> = ({
+  q,
+  qIndex,
+  opened,
+  values,
+  onToggle,
+  onChange,
+}) => {
+  const questionRef = useRef<HTMLDivElement>(null)
+
+  useClickOutside(questionRef, () => {
+    onToggle(qIndex, true)
+  })
+
+  return (
+    <div className="group">
+      <div className="group-label">
+        <p>{`${qIndex + 1}. ${q.question}`}</p>
+      </div>
+      <div
+        ref={questionRef}
+        className={classNames('group-select', {
+          open: opened,
+        })}>
+        <div className="label" onClick={() => onToggle(qIndex)}>
+          <p>{values[q.question] || q.placeholder}</p>
         </div>
-      ))}
+        <div className="option">
+          {q.answers.map((a, aIndex) => (
+            <div key={`a-${aIndex}`} className="expediting-answers">
+              <RadioElement<string>
+                className={classNames('expediting-answer-option', {
+                  // @ts-ignore
+                  'option-selected': values[q.question] === a.value,
+                })}
+                label={a.label}
+                name={q.question}
+                // @ts-ignore
+                selected={values[q.question] === a.value}
+                value={a.value}
+                onSelect={(v: string) => {
+                  onToggle(qIndex)
+                  onChange(q.question, v)
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
+
+const ExpeditingServiceQuestions: React.FC<Props> = ({ values, onChange }) => {
+  const [opened, setOpened] = useState<number[]>([])
+
+  const onToggle = useCallback(
+    (index: number, close?: boolean) => {
+      if (close) {
+        setOpened(opened.filter((o) => o !== index))
+      } else {
+        const isOpened = opened.includes(index)
+        if (isOpened) setOpened(opened.filter((o) => o !== index))
+        if (!isOpened) setOpened([...opened, index])
+      }
+    },
+    [opened],
+  )
+
+  return (
+    <div className="form-fields">
+      <div className="select-list">
+        {questions.map((q, qIndex) => (
+          <QuestionRow
+            key={`q-${qIndex}`}
+            q={q}
+            qIndex={qIndex}
+            opened={opened.includes(qIndex)}
+            values={values}
+            onToggle={onToggle}
+            onChange={onChange}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default ExpeditingServiceQuestions
